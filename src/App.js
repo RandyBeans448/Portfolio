09eddd1d3bab86672cd8b830e589b480ff56c50d
RandyBeans448 <dmncbishop@gmail.com>
2021-01-18 18:11:18 +0000

    prt9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
-
+import ParticlesContainer from "./components/Particles";
 import NavBar from "./components/NavBar";
+import Particles from "react-tsparticles";
 
 
 export default () => (
   <div className="container">
+    <ParticlesContainer/>
     <NavBar />
   </div>
 );
